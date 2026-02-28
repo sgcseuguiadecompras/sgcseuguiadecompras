25f3694d6ba6409033a54266f41ab0f05cf07449
SEU GUIA DE COMPRAS <sgc.seuguiadecompras@gmail.com>
2026-02-28 00:19:28 -0300

    Update route.js
    Adiciona callback de autenticação

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -1,1 +1,26 @@
+import { NextResponse } from 'next/server';
 
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get('code');
+  
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+  
+  const response = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code: code,
+    }),
+  });
+  
+  const data = await response.json();
+  
+  return NextResponse.redirect(`https://sgcseuguiadecompras.vercel.app/admin/#access_token=${data.access_token}&token_type=bearer`);
+}
