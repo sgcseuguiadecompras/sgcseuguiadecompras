aa71a7def603980dbcded5bdd46774c886ed06fd
SEU GUIA DE COMPRAS <sgc.seuguiadecompras@gmail.com>
2026-03-01 00:52:22 -0300

    Update route.ts

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,1 +1,10 @@
+import { NextResponse } from 'next/server';
 
+export async function GET() {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const redirectUri = `https://sgcseuguiadecompras.vercel.app/api/auth/callback`;
+
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo`;
+
+  return NextResponse.redirect(githubAuthUrl);
+}
